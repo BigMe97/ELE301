9daa9571fc3d2eb44b9adafd27a0767a8f6b3ca4
BigMe97 <magnuseide@msn.com>
2021-10-31 08:07:13 +0100

    Kortleser V1

---
+++ Kortleser/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace Kortleser
+{
+    partial class Kortleser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_num_1 = new System.Windows.Forms.Button();
+            this.txt_Kortlesernummer = new System.Windows.Forms.TextBox();
+            this.txt_KortID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.btn_num_2 = new System.Windows.Forms.Button();
+            this.btn_num_3 = new System.Windows.Forms.Button();
+            this.btn_num_4 = new System.Windows.Forms.Button();
+            this.btn_num_5 = new System.Windows.Forms.Button();
+            this.btn_num_6 = new System.Windows.Forms.Button();
+            this.btn_num_7 = new System.Windows.Forms.Button();
+            this.btn_num_8 = new System.Windows.Forms.Button();
+            this.btn_num_9 = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_num_0 = new System.Windows.Forms.Button();
+            this.btn_num_HASH = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_num_1
+            // 
+            this.btn_num_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_1.FlatAppearance.BorderSize = 3;
+            this.btn_num_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_1.Location = new System.Drawing.Point(29, 96);
+            this.btn_num_1.Name = "btn_num_1";
+            this.btn_num_1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_1.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_1.TabIndex = 2;
+            this.btn_num_1.Text = "1";
+            this.btn_num_1.UseVisualStyleBackColor = true;
+            this.btn_num_1.Click += new System.EventHandler(this.btn_num_1_Click);
+            // 
+            // txt_Kortlesernummer
+            // 
+            this.txt_Kortlesernummer.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Kortlesernummer.Location = new System.Drawing.Point(29, 45);
+            this.txt_Kortlesernummer.Name = "txt_Kortlesernummer";
+            this.txt_Kortlesernummer.Size = new System.Drawing.Size(93, 29);
+            this.txt_Kortlesernummer.TabIndex = 0;
+            this.txt_Kortlesernummer.TextChanged += new System.EventHandler(this.txt_Kortlesernummer_TextChanged);
+            // 
+            // txt_KortID
+            // 
+            this.txt_KortID.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_KortID.Location = new System.Drawing.Point(152, 45);
+            this.txt_KortID.Name = "txt_KortID";
+            this.txt_KortID.Size = new System.Drawing.Size(93, 29);
+            this.txt_KortID.TabIndex = 1;
+            this.txt_KortID.TextChanged += new System.EventHandler(this.txt_KortID_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Kortleser";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(148, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "KortID";
+            // 
+            // btn_num_2
+            // 
+            this.btn_num_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_2.FlatAppearance.BorderSize = 3;
+            this.btn_num_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_2.Location = new System.Drawing.Point(136, 96);
+            this.btn_num_2.Name = "btn_num_2";
+            this.btn_num_2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_2.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_2.TabIndex = 4;
+            this.btn_num_2.Text = "2";
+            this.btn_num_2.UseVisualStyleBackColor = true;
+            this.btn_num_2.Click += new System.EventHandler(this.btn_num_2_Click);
+            // 
+            // btn_num_3
+            // 
+            this.btn_num_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_3.FlatAppearance.BorderSize = 3;
+            this.btn_num_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_3.Location = new System.Drawing.Point(243, 96);
+            this.btn_num_3.Name = "btn_num_3";
+            this.btn_num_3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_3.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_3.TabIndex = 5;
+            this.btn_num_3.Text = "3";
+            this.btn_num_3.UseVisualStyleBackColor = true;
+            this.btn_num_3.Click += new System.EventHandler(this.btn_num_3_Click);
+            // 
+            // btn_num_4
+            // 
+            this.btn_num_4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_4.FlatAppearance.BorderSize = 3;
+            this.btn_num_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_4.Location = new System.Drawing.Point(29, 184);
+            this.btn_num_4.Name = "btn_num_4";
+            this.btn_num_4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_4.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_4.TabIndex = 6;
+            this.btn_num_4.Text = "4";
+            this.btn_num_4.UseVisualStyleBackColor = true;
+            this.btn_num_4.Click += new System.EventHandler(this.btn_num_4_Click);
+            // 
+            // btn_num_5
+            // 
+            this.btn_num_5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_5.FlatAppearance.BorderSize = 3;
+            this.btn_num_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_5.Location = new System.Drawing.Point(135, 184);
+            this.btn_num_5.Name = "btn_num_5";
+            this.btn_num_5.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_5.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_5.TabIndex = 7;
+            this.btn_num_5.Text = "5";
+            this.btn_num_5.UseVisualStyleBackColor = true;
+            this.btn_num_5.Click += new System.EventHandler(this.btn_num_5_Click);
+            // 
+            // btn_num_6
+            // 
+            this.btn_num_6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_6.FlatAppearance.BorderSize = 3;
+            this.btn_num_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_6.Location = new System.Drawing.Point(243, 184);
+            this.btn_num_6.Name = "btn_num_6";
+            this.btn_num_6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_6.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_6.TabIndex = 8;
+            this.btn_num_6.Text = "6";
+            this.btn_num_6.UseVisualStyleBackColor = true;
+            this.btn_num_6.Click += new System.EventHandler(this.btn_num_6_Click);
+            // 
+            // btn_num_7
+            // 
+            this.btn_num_7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_7.FlatAppearance.BorderSize = 3;
+            this.btn_num_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_7.Location = new System.Drawing.Point(29, 272);
+            this.btn_num_7.Name = "btn_num_7";
+            this.btn_num_7.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_7.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_7.TabIndex = 9;
+            this.btn_num_7.Text = "7";
+            this.btn_num_7.UseVisualStyleBackColor = true;
+            this.btn_num_7.Click += new System.EventHandler(this.btn_num_7_Click);
+            // 
+            // btn_num_8
+            // 
+            this.btn_num_8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_8.FlatAppearance.BorderSize = 3;
+            this.btn_num_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_8.Location = new System.Drawing.Point(136, 272);
+            this.btn_num_8.Name = "btn_num_8";
+            this.btn_num_8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_8.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_8.TabIndex = 10;
+            this.btn_num_8.Text = "8";
+            this.btn_num_8.UseVisualStyleBackColor = true;
+            this.btn_num_8.Click += new System.EventHandler(this.btn_num_8_Click);
+            // 
+            // btn_num_9
+            // 
+            this.btn_num_9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_9.FlatAppearance.BorderSize = 3;
+            this.btn_num_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_9.Location = new System.Drawing.Point(243, 272);
+            this.btn_num_9.Name = "btn_num_9";
+            this.btn_num_9.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_9.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_9.TabIndex = 11;
+            this.btn_num_9.Text = "9";
+            this.btn_num_9.UseVisualStyleBackColor = true;
+            this.btn_num_9.Click += new System.EventHandler(this.btn_num_9_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Clear.FlatAppearance.BorderSize = 3;
+            this.btn_Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Clear.Location = new System.Drawing.Point(29, 360);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_Clear.Size = new System.Drawing.Size(76, 55);
+            this.btn_Clear.TabIndex = 12;
+            this.btn_Clear.Text = "C";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_num_0
+            // 
+            this.btn_num_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_0.FlatAppearance.BorderSize = 3;
+            this.btn_num_0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_0.Location = new System.Drawing.Point(136, 360);
+            this.btn_num_0.Name = "btn_num_0";
+            this.btn_num_0.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_0.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_0.TabIndex = 13;
+            this.btn_num_0.Text = "0";
+            this.btn_num_0.UseVisualStyleBackColor = true;
+            this.btn_num_0.Click += new System.EventHandler(this.btn_num_0_Click);
+            // 
+            // btn_num_HASH
+            // 
+            this.btn_num_HASH.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_num_HASH.FlatAppearance.BorderSize = 3;
+            this.btn_num_HASH.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_num_HASH.Location = new System.Drawing.Point(243, 360);
+            this.btn_num_HASH.Name = "btn_num_HASH";
+            this.btn_num_HASH.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btn_num_HASH.Size = new System.Drawing.Size(76, 55);
+            this.btn_num_HASH.TabIndex = 14;
+            this.btn_num_HASH.Text = "#";
+            this.btn_num_HASH.UseVisualStyleBackColor = true;
+            this.btn_num_HASH.Click += new System.EventHandler(this.btn_num_HASH_Click);
+            // 
+            // Kortleser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 531);
+            this.Controls.Add(this.btn_num_HASH);
+            this.Controls.Add(this.btn_num_0);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_num_9);
+            this.Controls.Add(this.btn_num_8);
+            this.Controls.Add(this.btn_num_7);
+            this.Controls.Add(this.btn_num_6);
+            this.Controls.Add(this.btn_num_5);
+            this.Controls.Add(this.btn_num_4);
+            this.Controls.Add(this.btn_num_3);
+            this.Controls.Add(this.btn_num_2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_KortID);
+            this.Controls.Add(this.txt_Kortlesernummer);
+            this.Controls.Add(this.btn_num_1);
+            this.Name = "Kortleser";
+            this.Text = "Kortleser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Kortleser_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_num_1;
+        private System.Windows.Forms.TextBox txt_Kortlesernummer;
+        private System.Windows.Forms.TextBox txt_KortID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button btn_num_2;
+        private System.Windows.Forms.Button btn_num_3;
+        private System.Windows.Forms.Button btn_num_4;
+        private System.Windows.Forms.Button btn_num_5;
+        private System.Windows.Forms.Button btn_num_6;
+        private System.Windows.Forms.Button btn_num_7;
+        private System.Windows.Forms.Button btn_num_8;
+        private System.Windows.Forms.Button btn_num_9;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_num_0;
+        private System.Windows.Forms.Button btn_num_HASH;
+    }
+}
+
---
+++ Kortleser/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kortleser
+{
+    public partial class Kortleser : Form
+    {
+        static string passcode = "12345";
+
+
+
+        public Kortleser()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_Kortlesernummer_TextChanged(object sender, EventArgs e)
+        {
+            string txt = txt_Kortlesernummer.Text;
+
+            try
+            {
+                // Keep the string a number
+                if (txt.Length > 0) { Convert.ToInt32(txt); }
+
+                // Keep the string at 4 digits
+                if (txt.Length > 4)
+                {
+                    txt_Kortlesernummer.Text = txt.Substring(0, 4);
+                    txt_Kortlesernummer.SelectionStart = 4;
+                    // throw new InvalidOperationException("Kortnummer kan bare være 4 tegn");
+
+                }
+
+            }
+            //catch (InvalidExpressionException not_int)
+            //{
+            //    txt_Kortlesernummer.Text = txt.Substring(0, txt.Length-1);
+            //    MessageBox.Show(not_int.Message);
+            //}
+            catch (Exception i)
+            {
+                txt_Kortlesernummer.Text = txt.Substring(0, txt.Length - 1);
+                MessageBox.Show(i.Message);
+            }
+
+
+
+        }
+
+
+
+        private void txt_KortID_TextChanged(object sender, EventArgs e)
+        {
+            string txt = txt_KortID.Text;
+
+            try
+            {
+                // Keep the string a number
+                if (txt.Length > 0) { Convert.ToInt32(txt); }
+
+                // Keep the string at 4 digits
+                if (txt.Length > 4)
+                {
+                    txt_KortID.Text = txt.Substring(0, 4);
+                    txt_KortID.SelectionStart = 4;
+                    // throw new InvalidOperationException("Kortnummer kan bare være 4 tegn");
+
+                }
+
+            }
+            //catch (InvalidExpressionException not_int)
+            //{
+            //    txt_KortID.Text = txt.Substring(0, txt.Length - 1);
+            //    MessageBox.Show(not_int.Message);
+            //}
+            catch (Exception i)
+            {
+                txt_KortID.Text = txt.Substring(0, txt.Length - 1);
+                MessageBox.Show(i.Message);
+            }
+        }
+
+
+
+        private void btn_num_1_Click(object sender, EventArgs e)
+        {
+            TM_kode('1');
+        }
+
+        private void btn_num_2_Click(object sender, EventArgs e)
+        {
+            TM_kode('2');
+        }
+
+        private void btn_num_3_Click(object sender, EventArgs e)
+        {
+            TM_kode('3');
+        }
+
+        private void btn_num_4_Click(object sender, EventArgs e)
+        {
+            TM_kode('4');
+        }
+
+        private void btn_num_5_Click(object sender, EventArgs e)
+        {
+            TM_kode('5');
+        }
+
+        private void btn_num_6_Click(object sender, EventArgs e)
+        {
+            TM_kode('6');
+        }
+
+        private void btn_num_7_Click(object sender, EventArgs e)
+        {
+            TM_kode('7');
+        }
+
+        private void btn_num_8_Click(object sender, EventArgs e)
+        {
+            TM_kode('8');
+        }
+
+        private void btn_num_9_Click(object sender, EventArgs e)
+        {
+            TM_kode('9');
+        }
+
+        private void btn_num_0_Click(object sender, EventArgs e)
+        {
+            TM_kode('0');
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            TM_kode('C');
+        }
+
+
+        // Veit ikke hva/om vi skal bruke den til noe
+        private void btn_num_HASH_Click(object sender, EventArgs e)
+        {
+            TM_kode('#');
+        }
+
+
+        static void TM_kode(char input)
+        {
+            switch (passcode.Length)
+            {
+                case 5:
+                    passcode = input.ToString();
+                    break;
+                case 1:
+                    passcode = passcode+input;
+                    break;
+
+                case 2:
+                    passcode = passcode + input;
+                    break;
+
+                case 3:
+                    passcode = passcode + input;
+                    Send(passcode);
+                    passcode = "12345";
+                    break;
+
+                default:
+                    MessageBox.Show("Noe feilet");
+                    passcode = "12345";
+                    break;
+            }
+
+        }
+
+
+        static void Send(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void Kortleser_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Programmet vil avslutte...");
+        }
+    }
+}
